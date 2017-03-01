8db23b7ba00a952fd7a70401ba34b1e677c543f2
EMaintz <Emil@maintz-2.local>
2017-03-01 21:50:36 +0100

    Initial Commit

---
+++ TravelSplit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelSplit
+//
+//  Created by Emil Maintz on 01/03/2017.
+//  Copyright © 2017 Emil Maintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
